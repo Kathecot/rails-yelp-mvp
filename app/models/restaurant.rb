9f1c6d2256daaa7efd5bdc7f4e6d9cd596bc05c4
Katherine Cornejo <kathe.cornejo@gmail.com>
2022-01-19 17:20:37 -0500

    restaurant controller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
+  # validates
+  # name cannot be blank
+  validates :name, presence: true
+  # address cannot be blank
+  validates :address, presence: true
+  # category cannot be blank
+  # neptunian is not a valid category
+  validates :category, presence: true, inclusion: { in: %w[chinese italian japanese french belgian] }
 end
